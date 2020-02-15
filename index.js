42124f0d483ce6fc31dea3d033acf2c91b6f8683
Luca Cataldo <35984346+lucacataldo@users.noreply.github.com>
2020-02-15 01:16:32 -0500

    proof of concept: show products on store page

--- index.js
+++ index.js
@@ -169,9 +169,12 @@ app.get("/sites/:id", (req, res) => {
 	userService
 		.getWebsiteDataById(id)
 		.then(site => {
-			site.customMessage = "hello";
-			site.baseUrl = "/sites/" + site._id;
-			res.render("siteViews/home", { layout: false, siteData: site });
+			productService.getAllProducts().then((prods) => {
+				site.customMessage = "hello";
+				site.baseUrl = "/sites/" + site._id;
+				res.render("siteViews/home", { layout: false, siteData: site, prods: prods });
+			})
+
 		})
 		.catch(err => {
 			res.redirect("/404");
@@ -218,10 +221,10 @@ app.post("/signup", (req, res) => {
 		});
 });
 
-app.post("/resetPassword", function(req, res) {
+app.post("/resetPassword", function (req, res) {
 	const email = req.body.email;
 
-	userService.findMatchingEmail(email).then(function(user) {
+	userService.findMatchingEmail(email).then(function (user) {
 		if (user) {
 			mailService
 				.sendVerificationEmail(req.body.email, "reset")
