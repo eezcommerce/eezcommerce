55d3e76b7559f582065722339f2231a64a2f4c10
John Huynh <jhhuynh1@myseneca.ca>
2020-03-11 11:48:19 -0400

    .val

--- modules/orderService.js
+++ modules/orderService.js
@@ -36,7 +36,6 @@ module.exports.getOrderById = oneId => {
 		Orders.findOne({ _id: oneId }, (err, ords) => {
 			if (!err) {
 				resolve(ords);
-
 			} else {
 				console.log("error:" + err);
 				reject(err);
