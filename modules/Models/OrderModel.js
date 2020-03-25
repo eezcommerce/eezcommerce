bf82083008c6837cf3bff4602927a64149b3ddb4
John Huynh <jhhuynh1@myseneca.ca>
2020-03-25 16:43:25 -0400

    multipleof

--- modules/Models/OrderModel.js
+++ modules/Models/OrderModel.js
@@ -6,11 +6,25 @@ const OrdersModel = mongoose.model(
 		SellerID: { type: String },
 		CustName: { type: String },
 		destAddress: { type: String },
+		CustEmail: {			
+			type: String,
+			maxlength: 256,
+			minlength: 4,
+			required: true,
+			unique: true 
+		},
 		status: { type: String },
-		total: { type: String },
+		subtotal: {type: Number },
+		total: { type: Number, 
+			multipleOf: 0.01
+		},
 		ProductList: [{ ProductID: String, ProductName: String, Qty: Number }],
 		created_at: { type: Date, required: true, default: Date.now },
-		updated_at: { type: Date, required: true, default: Date.now }
+		updated_at: { type: Date, required: true, default: Date.now },
+		specInstr: {
+			type: String,
+			maxlength: 256
+		}
 	})
 );
 
